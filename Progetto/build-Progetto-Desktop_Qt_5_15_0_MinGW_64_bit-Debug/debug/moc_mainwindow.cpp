625deec03d011955ae0e0fb37de5b2bf856159a0
Vittorio M <78265579+vittmatr@users.noreply.github.com>
2021-01-30 15:47:31 +0100

    Add files via upload

---
+++ Progetto/build-Progetto-Desktop_Qt_5_15_0_MinGW_64_bit-Debug/debug/moc_mainwindow.cpp
@@ -0,0 +1,310 @@
+/****************************************************************************
+** Meta object code from reading C++ file 'mainwindow.h'
+**
+** Created by: The Qt Meta Object Compiler version 67 (Qt 5.15.0)
+**
+** WARNING! All changes made in this file will be lost!
+*****************************************************************************/
+
+#include <memory>
+#include "../../Progetto/mainwindow.h"
+#include <QtCore/qbytearray.h>
+#include <QtCore/qmetatype.h>
+#if !defined(Q_MOC_OUTPUT_REVISION)
+#error "The header file 'mainwindow.h' doesn't include <QObject>."
+#elif Q_MOC_OUTPUT_REVISION != 67
+#error "This file was generated using the moc from 5.15.0. It"
+#error "cannot be used with the include files from this version of Qt."
+#error "(The moc has changed too much.)"
+#endif
+
+QT_BEGIN_MOC_NAMESPACE
+QT_WARNING_PUSH
+QT_WARNING_DISABLE_DEPRECATED
+struct qt_meta_stringdata_MainWindow_t {
+    QByteArrayData data[42];
+    char stringdata0[1145];
+};
+#define QT_MOC_LITERAL(idx, ofs, len) \
+    Q_STATIC_BYTE_ARRAY_DATA_HEADER_INITIALIZER_WITH_OFFSET(len, \
+    qptrdiff(offsetof(qt_meta_stringdata_MainWindow_t, stringdata0) + ofs \
+        - idx * sizeof(QByteArrayData)) \
+    )
+static const qt_meta_stringdata_MainWindow_t qt_meta_stringdata_MainWindow = {
+    {
+QT_MOC_LITERAL(0, 0, 10), // "MainWindow"
+QT_MOC_LITERAL(1, 11, 18), // "on_backInserimento"
+QT_MOC_LITERAL(2, 30, 0), // ""
+QT_MOC_LITERAL(3, 31, 14), // "on_backGestore"
+QT_MOC_LITERAL(4, 46, 24), // "on_bottoneAutore_clicked"
+QT_MOC_LITERAL(5, 71, 27), // "on_bottoneAfferenza_clicked"
+QT_MOC_LITERAL(6, 99, 26), // "on_bottoneGestione_clicked"
+QT_MOC_LITERAL(7, 126, 29), // "on_bottoneInserimento_clicked"
+QT_MOC_LITERAL(8, 156, 25), // "on_bottoneRivista_clicked"
+QT_MOC_LITERAL(9, 182, 25), // "on_aggiornaAutori_clicked"
+QT_MOC_LITERAL(10, 208, 24), // "on_autoreGestore_clicked"
+QT_MOC_LITERAL(11, 233, 29), // "on_aggiornaConferenze_clicked"
+QT_MOC_LITERAL(12, 263, 26), // "on_aggiornaRiviste_clicked"
+QT_MOC_LITERAL(13, 290, 26), // "on_bottoneArticolo_clicked"
+QT_MOC_LITERAL(14, 317, 26), // "on_bottoneOkAutori_clicked"
+QT_MOC_LITERAL(15, 344, 27), // "on_bottoneOkKeyword_clicked"
+QT_MOC_LITERAL(16, 372, 35), // "on_bottoneArticoloCorrelato_c..."
+QT_MOC_LITERAL(17, 408, 27), // "on_aggiornaArticoli_clicked"
+QT_MOC_LITERAL(18, 436, 28), // "on_bottoneConferenza_clicked"
+QT_MOC_LITERAL(19, 465, 22), // "on_homeGestore_clicked"
+QT_MOC_LITERAL(20, 488, 21), // "on_pushButton_clicked"
+QT_MOC_LITERAL(21, 510, 33), // "on_bottoneRivista_Ricerca_cli..."
+QT_MOC_LITERAL(22, 544, 33), // "on_bottoneKeyword_Ricerca_cli..."
+QT_MOC_LITERAL(23, 578, 24), // "on_annullaAutore_clicked"
+QT_MOC_LITERAL(24, 603, 28), // "on_annullaConferenza_clicked"
+QT_MOC_LITERAL(25, 632, 25), // "on_annullaRivista_clicked"
+QT_MOC_LITERAL(26, 658, 26), // "on_annullaArticolo_clicked"
+QT_MOC_LITERAL(27, 685, 30), // "on_bottone_mediaAutori_clicked"
+QT_MOC_LITERAL(28, 716, 21), // "on_backToPag1_clicked"
+QT_MOC_LITERAL(29, 738, 20), // "on_goToPage2_clicked"
+QT_MOC_LITERAL(30, 759, 36), // "on_bottone_keywordRedditizie_..."
+QT_MOC_LITERAL(31, 796, 39), // "on_bottone_ArticoliAutorePrez..."
+QT_MOC_LITERAL(32, 836, 43), // "on_bottone_ArticoliAutorePrez..."
+QT_MOC_LITERAL(33, 880, 21), // "on_backToPag2_clicked"
+QT_MOC_LITERAL(34, 902, 20), // "on_goToPage3_clicked"
+QT_MOC_LITERAL(35, 923, 49), // "on_bottone_ArticoliKeywordAnn..."
+QT_MOC_LITERAL(36, 973, 25), // "on_importa_autori_clicked"
+QT_MOC_LITERAL(37, 999, 29), // "on_importa_conferenze_clicked"
+QT_MOC_LITERAL(38, 1029, 26), // "on_importa_riviste_clicked"
+QT_MOC_LITERAL(39, 1056, 28), // "on_bottoneConferenze_clicked"
+QT_MOC_LITERAL(40, 1085, 31), // "on_bottoneOrganizzatori_clicked"
+QT_MOC_LITERAL(41, 1117, 27) // "on_importa_articoli_clicked"
+
+    },
+    "MainWindow\0on_backInserimento\0\0"
+    "on_backGestore\0on_bottoneAutore_clicked\0"
+    "on_bottoneAfferenza_clicked\0"
+    "on_bottoneGestione_clicked\0"
+    "on_bottoneInserimento_clicked\0"
+    "on_bottoneRivista_clicked\0"
+    "on_aggiornaAutori_clicked\0"
+    "on_autoreGestore_clicked\0"
+    "on_aggiornaConferenze_clicked\0"
+    "on_aggiornaRiviste_clicked\0"
+    "on_bottoneArticolo_clicked\0"
+    "on_bottoneOkAutori_clicked\0"
+    "on_bottoneOkKeyword_clicked\0"
+    "on_bottoneArticoloCorrelato_clicked\0"
+    "on_aggiornaArticoli_clicked\0"
+    "on_bottoneConferenza_clicked\0"
+    "on_homeGestore_clicked\0on_pushButton_clicked\0"
+    "on_bottoneRivista_Ricerca_clicked\0"
+    "on_bottoneKeyword_Ricerca_clicked\0"
+    "on_annullaAutore_clicked\0"
+    "on_annullaConferenza_clicked\0"
+    "on_annullaRivista_clicked\0"
+    "on_annullaArticolo_clicked\0"
+    "on_bottone_mediaAutori_clicked\0"
+    "on_backToPag1_clicked\0on_goToPage2_clicked\0"
+    "on_bottone_keywordRedditizie_clicked\0"
+    "on_bottone_ArticoliAutorePrezzo_clicked\0"
+    "on_bottone_ArticoliAutorePrezzoAlto_clicked\0"
+    "on_backToPag2_clicked\0on_goToPage3_clicked\0"
+    "on_bottone_ArticoliKeywordAnnoDecrescente_clicked\0"
+    "on_importa_autori_clicked\0"
+    "on_importa_conferenze_clicked\0"
+    "on_importa_riviste_clicked\0"
+    "on_bottoneConferenze_clicked\0"
+    "on_bottoneOrganizzatori_clicked\0"
+    "on_importa_articoli_clicked"
+};
+#undef QT_MOC_LITERAL
+
+static const uint qt_meta_data_MainWindow[] = {
+
+ // content:
+       8,       // revision
+       0,       // classname
+       0,    0, // classinfo
+      40,   14, // methods
+       0,    0, // properties
+       0,    0, // enums/sets
+       0,    0, // constructors
+       0,       // flags
+       0,       // signalCount
+
+ // slots: name, argc, parameters, tag, flags
+       1,    0,  214,    2, 0x08 /* Private */,
+       3,    0,  215,    2, 0x08 /* Private */,
+       4,    0,  216,    2, 0x08 /* Private */,
+       5,    0,  217,    2, 0x08 /* Private */,
+       6,    0,  218,    2, 0x08 /* Private */,
+       7,    0,  219,    2, 0x08 /* Private */,
+       8,    0,  220,    2, 0x08 /* Private */,
+       9,    0,  221,    2, 0x08 /* Private */,
+      10,    0,  222,    2, 0x08 /* Private */,
+      11,    0,  223,    2, 0x08 /* Private */,
+      12,    0,  224,    2, 0x08 /* Private */,
+      13,    0,  225,    2, 0x08 /* Private */,
+      14,    0,  226,    2, 0x08 /* Private */,
+      15,    0,  227,    2, 0x08 /* Private */,
+      16,    0,  228,    2, 0x08 /* Private */,
+      17,    0,  229,    2, 0x08 /* Private */,
+      18,    0,  230,    2, 0x08 /* Private */,
+      19,    0,  231,    2, 0x08 /* Private */,
+      20,    0,  232,    2, 0x08 /* Private */,
+      21,    0,  233,    2, 0x08 /* Private */,
+      22,    0,  234,    2, 0x08 /* Private */,
+      23,    0,  235,    2, 0x08 /* Private */,
+      24,    0,  236,    2, 0x08 /* Private */,
+      25,    0,  237,    2, 0x08 /* Private */,
+      26,    0,  238,    2, 0x08 /* Private */,
+      27,    0,  239,    2, 0x08 /* Private */,
+      28,    0,  240,    2, 0x08 /* Private */,
+      29,    0,  241,    2, 0x08 /* Private */,
+      30,    0,  242,    2, 0x08 /* Private */,
+      31,    0,  243,    2, 0x08 /* Private */,
+      32,    0,  244,    2, 0x08 /* Private */,
+      33,    0,  245,    2, 0x08 /* Private */,
+      34,    0,  246,    2, 0x08 /* Private */,
+      35,    0,  247,    2, 0x08 /* Private */,
+      36,    0,  248,    2, 0x08 /* Private */,
+      37,    0,  249,    2, 0x08 /* Private */,
+      38,    0,  250,    2, 0x08 /* Private */,
+      39,    0,  251,    2, 0x08 /* Private */,
+      40,    0,  252,    2, 0x08 /* Private */,
+      41,    0,  253,    2, 0x08 /* Private */,
+
+ // slots: parameters
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+
+       0        // eod
+};
+
+void MainWindow::qt_static_metacall(QObject *_o, QMetaObject::Call _c, int _id, void **_a)
+{
+    if (_c == QMetaObject::InvokeMetaMethod) {
+        auto *_t = static_cast<MainWindow *>(_o);
+        Q_UNUSED(_t)
+        switch (_id) {
+        case 0: _t->on_backInserimento(); break;
+        case 1: _t->on_backGestore(); break;
+        case 2: _t->on_bottoneAutore_clicked(); break;
+        case 3: _t->on_bottoneAfferenza_clicked(); break;
+        case 4: _t->on_bottoneGestione_clicked(); break;
+        case 5: _t->on_bottoneInserimento_clicked(); break;
+        case 6: _t->on_bottoneRivista_clicked(); break;
+        case 7: _t->on_aggiornaAutori_clicked(); break;
+        case 8: _t->on_autoreGestore_clicked(); break;
+        case 9: _t->on_aggiornaConferenze_clicked(); break;
+        case 10: _t->on_aggiornaRiviste_clicked(); break;
+        case 11: _t->on_bottoneArticolo_clicked(); break;
+        case 12: _t->on_bottoneOkAutori_clicked(); break;
+        case 13: _t->on_bottoneOkKeyword_clicked(); break;
+        case 14: _t->on_bottoneArticoloCorrelato_clicked(); break;
+        case 15: _t->on_aggiornaArticoli_clicked(); break;
+        case 16: _t->on_bottoneConferenza_clicked(); break;
+        case 17: _t->on_homeGestore_clicked(); break;
+        case 18: _t->on_pushButton_clicked(); break;
+        case 19: _t->on_bottoneRivista_Ricerca_clicked(); break;
+        case 20: _t->on_bottoneKeyword_Ricerca_clicked(); break;
+        case 21: _t->on_annullaAutore_clicked(); break;
+        case 22: _t->on_annullaConferenza_clicked(); break;
+        case 23: _t->on_annullaRivista_clicked(); break;
+        case 24: _t->on_annullaArticolo_clicked(); break;
+        case 25: _t->on_bottone_mediaAutori_clicked(); break;
+        case 26: _t->on_backToPag1_clicked(); break;
+        case 27: _t->on_goToPage2_clicked(); break;
+        case 28: _t->on_bottone_keywordRedditizie_clicked(); break;
+        case 29: _t->on_bottone_ArticoliAutorePrezzo_clicked(); break;
+        case 30: _t->on_bottone_ArticoliAutorePrezzoAlto_clicked(); break;
+        case 31: _t->on_backToPag2_clicked(); break;
+        case 32: _t->on_goToPage3_clicked(); break;
+        case 33: _t->on_bottone_ArticoliKeywordAnnoDecrescente_clicked(); break;
+        case 34: _t->on_importa_autori_clicked(); break;
+        case 35: _t->on_importa_conferenze_clicked(); break;
+        case 36: _t->on_importa_riviste_clicked(); break;
+        case 37: _t->on_bottoneConferenze_clicked(); break;
+        case 38: _t->on_bottoneOrganizzatori_clicked(); break;
+        case 39: _t->on_importa_articoli_clicked(); break;
+        default: ;
+        }
+    }
+    Q_UNUSED(_a);
+}
+
+QT_INIT_METAOBJECT const QMetaObject MainWindow::staticMetaObject = { {
+    QMetaObject::SuperData::link<QMainWindow::staticMetaObject>(),
+    qt_meta_stringdata_MainWindow.data,
+    qt_meta_data_MainWindow,
+    qt_static_metacall,
+    nullptr,
+    nullptr
+} };
+
+
+const QMetaObject *MainWindow::metaObject() const
+{
+    return QObject::d_ptr->metaObject ? QObject::d_ptr->dynamicMetaObject() : &staticMetaObject;
+}
+
+void *MainWindow::qt_metacast(const char *_clname)
+{
+    if (!_clname) return nullptr;
+    if (!strcmp(_clname, qt_meta_stringdata_MainWindow.stringdata0))
+        return static_cast<void*>(this);
+    return QMainWindow::qt_metacast(_clname);
+}
+
+int MainWindow::qt_metacall(QMetaObject::Call _c, int _id, void **_a)
+{
+    _id = QMainWindow::qt_metacall(_c, _id, _a);
+    if (_id < 0)
+        return _id;
+    if (_c == QMetaObject::InvokeMetaMethod) {
+        if (_id < 40)
+            qt_static_metacall(this, _c, _id, _a);
+        _id -= 40;
+    } else if (_c == QMetaObject::RegisterMethodArgumentMetaType) {
+        if (_id < 40)
+            *reinterpret_cast<int*>(_a[0]) = -1;
+        _id -= 40;
+    }
+    return _id;
+}
+QT_WARNING_POP
+QT_END_MOC_NAMESPACE
---
+++ Progetto/build-Progetto-Desktop_Qt_5_2_1_clang_64bit-Debug/moc_mainwindow.cpp
@@ -0,0 +1,234 @@
+/****************************************************************************
+** Meta object code from reading C++ file 'mainwindow.h'
+**
+** Created by: The Qt Meta Object Compiler version 67 (Qt 5.2.1)
+**
+** WARNING! All changes made in this file will be lost!
+*****************************************************************************/
+
+#include "../Progetto/mainwindow.h"
+#include <QtCore/qbytearray.h>
+#include <QtCore/qmetatype.h>
+#if !defined(Q_MOC_OUTPUT_REVISION)
+#error "The header file 'mainwindow.h' doesn't include <QObject>."
+#elif Q_MOC_OUTPUT_REVISION != 67
+#error "This file was generated using the moc from 5.2.1. It"
+#error "cannot be used with the include files from this version of Qt."
+#error "(The moc has changed too much.)"
+#endif
+
+QT_BEGIN_MOC_NAMESPACE
+struct qt_meta_stringdata_MainWindow_t {
+    QByteArrayData data[28];
+    char stringdata[745];
+};
+#define QT_MOC_LITERAL(idx, ofs, len) \
+    Q_STATIC_BYTE_ARRAY_DATA_HEADER_INITIALIZER_WITH_OFFSET(len, \
+    offsetof(qt_meta_stringdata_MainWindow_t, stringdata) + ofs \
+        - idx * sizeof(QByteArrayData) \
+    )
+static const qt_meta_stringdata_MainWindow_t qt_meta_stringdata_MainWindow = {
+    {
+QT_MOC_LITERAL(0, 0, 10),
+QT_MOC_LITERAL(1, 11, 24),
+QT_MOC_LITERAL(2, 36, 0),
+QT_MOC_LITERAL(3, 37, 27),
+QT_MOC_LITERAL(4, 65, 26),
+QT_MOC_LITERAL(5, 92, 29),
+QT_MOC_LITERAL(6, 122, 26),
+QT_MOC_LITERAL(7, 149, 29),
+QT_MOC_LITERAL(8, 179, 25),
+QT_MOC_LITERAL(9, 205, 25),
+QT_MOC_LITERAL(10, 231, 24),
+QT_MOC_LITERAL(11, 256, 29),
+QT_MOC_LITERAL(12, 286, 26),
+QT_MOC_LITERAL(13, 313, 26),
+QT_MOC_LITERAL(14, 340, 26),
+QT_MOC_LITERAL(15, 367, 27),
+QT_MOC_LITERAL(16, 395, 35),
+QT_MOC_LITERAL(17, 431, 27),
+QT_MOC_LITERAL(18, 459, 28),
+QT_MOC_LITERAL(19, 488, 27),
+QT_MOC_LITERAL(20, 516, 21),
+QT_MOC_LITERAL(21, 538, 33),
+QT_MOC_LITERAL(22, 572, 33),
+QT_MOC_LITERAL(23, 606, 24),
+QT_MOC_LITERAL(24, 631, 28),
+QT_MOC_LITERAL(25, 660, 25),
+QT_MOC_LITERAL(26, 686, 26),
+QT_MOC_LITERAL(27, 713, 30)
+    },
+    "MainWindow\0on_bottoneAutore_clicked\0"
+    "\0on_bottoneAfferenza_clicked\0"
+    "on_bottoneAutore_2_clicked\0"
+    "on_bottoneAfferenza_2_clicked\0"
+    "on_bottoneGestione_clicked\0"
+    "on_bottoneInserimento_clicked\0"
+    "on_bottoneRivista_clicked\0"
+    "on_aggiornaAutori_clicked\0"
+    "on_autoreGestore_clicked\0"
+    "on_aggiornaConferenze_clicked\0"
+    "on_aggiornaRiviste_clicked\0"
+    "on_bottoneArticolo_clicked\0"
+    "on_bottoneOkAutori_clicked\0"
+    "on_bottoneOkKeyword_clicked\0"
+    "on_bottoneArticoloCorrelato_clicked\0"
+    "on_aggiornaArticoli_clicked\0"
+    "on_bottoneConferenza_clicked\0"
+    "on_tornaInserimento_clicked\0"
+    "on_pushButton_clicked\0"
+    "on_bottoneRivista_Ricerca_clicked\0"
+    "on_bottoneKeyword_Ricerca_clicked\0"
+    "on_annullaAutore_clicked\0"
+    "on_annullaConferenza_clicked\0"
+    "on_annullaRivista_clicked\0"
+    "on_annullaArticolo_clicked\0"
+    "on_bottone_mediaAutori_clicked\0"
+};
+#undef QT_MOC_LITERAL
+
+static const uint qt_meta_data_MainWindow[] = {
+
+ // content:
+       7,       // revision
+       0,       // classname
+       0,    0, // classinfo
+      26,   14, // methods
+       0,    0, // properties
+       0,    0, // enums/sets
+       0,    0, // constructors
+       0,       // flags
+       0,       // signalCount
+
+ // slots: name, argc, parameters, tag, flags
+       1,    0,  144,    2, 0x08,
+       3,    0,  145,    2, 0x08,
+       4,    0,  146,    2, 0x08,
+       5,    0,  147,    2, 0x08,
+       6,    0,  148,    2, 0x08,
+       7,    0,  149,    2, 0x08,
+       8,    0,  150,    2, 0x08,
+       9,    0,  151,    2, 0x08,
+      10,    0,  152,    2, 0x08,
+      11,    0,  153,    2, 0x08,
+      12,    0,  154,    2, 0x08,
+      13,    0,  155,    2, 0x08,
+      14,    0,  156,    2, 0x08,
+      15,    0,  157,    2, 0x08,
+      16,    0,  158,    2, 0x08,
+      17,    0,  159,    2, 0x08,
+      18,    0,  160,    2, 0x08,
+      19,    0,  161,    2, 0x08,
+      20,    0,  162,    2, 0x08,
+      21,    0,  163,    2, 0x08,
+      22,    0,  164,    2, 0x08,
+      23,    0,  165,    2, 0x08,
+      24,    0,  166,    2, 0x08,
+      25,    0,  167,    2, 0x08,
+      26,    0,  168,    2, 0x08,
+      27,    0,  169,    2, 0x08,
+
+ // slots: parameters
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+
+       0        // eod
+};
+
+void MainWindow::qt_static_metacall(QObject *_o, QMetaObject::Call _c, int _id, void **_a)
+{
+    if (_c == QMetaObject::InvokeMetaMethod) {
+        MainWindow *_t = static_cast<MainWindow *>(_o);
+        switch (_id) {
+        case 0: _t->on_bottoneAutore_clicked(); break;
+        case 1: _t->on_bottoneAfferenza_clicked(); break;
+        case 2: _t->on_bottoneAutore_2_clicked(); break;
+        case 3: _t->on_bottoneAfferenza_2_clicked(); break;
+        case 4: _t->on_bottoneGestione_clicked(); break;
+        case 5: _t->on_bottoneInserimento_clicked(); break;
+        case 6: _t->on_bottoneRivista_clicked(); break;
+        case 7: _t->on_aggiornaAutori_clicked(); break;
+        case 8: _t->on_autoreGestore_clicked(); break;
+        case 9: _t->on_aggiornaConferenze_clicked(); break;
+        case 10: _t->on_aggiornaRiviste_clicked(); break;
+        case 11: _t->on_bottoneArticolo_clicked(); break;
+        case 12: _t->on_bottoneOkAutori_clicked(); break;
+        case 13: _t->on_bottoneOkKeyword_clicked(); break;
+        case 14: _t->on_bottoneArticoloCorrelato_clicked(); break;
+        case 15: _t->on_aggiornaArticoli_clicked(); break;
+        case 16: _t->on_bottoneConferenza_clicked(); break;
+        case 17: _t->on_tornaInserimento_clicked(); break;
+        case 18: _t->on_pushButton_clicked(); break;
+        case 19: _t->on_bottoneRivista_Ricerca_clicked(); break;
+        case 20: _t->on_bottoneKeyword_Ricerca_clicked(); break;
+        case 21: _t->on_annullaAutore_clicked(); break;
+        case 22: _t->on_annullaConferenza_clicked(); break;
+        case 23: _t->on_annullaRivista_clicked(); break;
+        case 24: _t->on_annullaArticolo_clicked(); break;
+        case 25: _t->on_bottone_mediaAutori_clicked(); break;
+        default: ;
+        }
+    }
+    Q_UNUSED(_a);
+}
+
+const QMetaObject MainWindow::staticMetaObject = {
+    { &QMainWindow::staticMetaObject, qt_meta_stringdata_MainWindow.data,
+      qt_meta_data_MainWindow,  qt_static_metacall, 0, 0}
+};
+
+
+const QMetaObject *MainWindow::metaObject() const
+{
+    return QObject::d_ptr->metaObject ? QObject::d_ptr->dynamicMetaObject() : &staticMetaObject;
+}
+
+void *MainWindow::qt_metacast(const char *_clname)
+{
+    if (!_clname) return 0;
+    if (!strcmp(_clname, qt_meta_stringdata_MainWindow.stringdata))
+        return static_cast<void*>(const_cast< MainWindow*>(this));
+    return QMainWindow::qt_metacast(_clname);
+}
+
+int MainWindow::qt_metacall(QMetaObject::Call _c, int _id, void **_a)
+{
+    _id = QMainWindow::qt_metacall(_c, _id, _a);
+    if (_id < 0)
+        return _id;
+    if (_c == QMetaObject::InvokeMetaMethod) {
+        if (_id < 26)
+            qt_static_metacall(this, _c, _id, _a);
+        _id -= 26;
+    } else if (_c == QMetaObject::RegisterMethodArgumentMetaType) {
+        if (_id < 26)
+            *reinterpret_cast<int*>(_a[0]) = -1;
+        _id -= 26;
+    }
+    return _id;
+}
+QT_END_MOC_NAMESPACE
